d66d4e3bd3c26f6ac2f237a6d5ba30d6a77359be
Ly Nguyen <Ly900@aol.com>
2017-08-15 20:26:57 -0400

    Change styling of font to remove flicker; Fixing gitignore

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,11 @@
+"use strict";
 
+var counter = 0,
+    gameBox = document.getElementsByClassName("square");
+//Each click adds to counter
+
+  //If odd click, it's Player 1
+    //Display an X.
+
+  //If even click, it's Player 2
+    //Display an O.
