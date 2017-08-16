c62105975388282da1fd1abfb2b5c8d1b78f8ac8
Ly Nguyen <Ly900@aol.com>
2017-08-15 22:28:30 -0400

    Adds letter based on if counter is even or odd, player 1 or player 2.

--- js/myjs.js
+++ js/myjs.js
@@ -1,11 +1,29 @@
 "use strict";
 
 var counter = 0,
-    gameBox = document.getElementsByClassName("square");
+  gameContainer = document.getElementsByClassName("main-game-div")[0];
+
 //Each click adds to counter
+gameContainer.addEventListener("click", markSquare);
 
-  //If odd click, it's Player 1
-    //Display an X.
+function increaseCounter() {
+  counter++;
+}
 
-  //If even click, it's Player 2
-    //Display an O.
+function markSquare(e) {
+  if (counter % 2 != 0) {
+    console.log("Player 1");
+    e.target.innerHTML = "X";
+  } else {
+    console.log("Player 2");
+    e.target.innerHTML = "O";
+  }
+
+  increaseCounter();
+}
+
+//If odd click, it's Player 1
+//Display an X.
+
+//If even click, it's Player 2
+//Display an O.
