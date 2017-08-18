d8b39e753385318a1f8af08effbcb8faba6cab2e
Ly Nguyen <Ly900@aol.com>
2017-08-17 21:07:48 -0400

    Adds card ID to player's array

--- js/myjs.js
+++ js/myjs.js
@@ -7,7 +7,7 @@ var gameCard = (function() {
   var filled = false,
     filledMark = null,
     counter = 0,
-    currentPlayer = undefined;
+    currentPlayer = 1;
 
   function startGame() {
     console.log("Game started by card!");
@@ -20,45 +20,79 @@ var gameCard = (function() {
 
   function isCardFilled() {
     var cardClicked = event.target,
-    cardAleadyFilled = cardClicked.classList.contains("filled");
+      cardAleadyFilled = cardClicked.classList.contains("filled"),
+      // The span below contains the id of the card to use in the player's array to check for win.
+      boxWithId = cardClicked.children[0],
+      cardId = boxWithId.id;
+    console.log("Card id: " + cardId);
 
     if (cardAleadyFilled) {
       console.log("This card has already been filled!");
+
+      // If card is NOT yet filled...
     } else {
-      markCard(cardClicked);
+      addClickedCardStyles(cardClicked);
+      increaseCounter();
+      markCard(cardClicked, boxWithId);
     }
   }
 
-  function markCard(cardClicked) {
+  // Changes background to light gray/white only.
+  function addClickedCardStyles(cardClicked) {
     cardClicked.classList.add("filled");
-    if (choosePlayer() === 1) {
-      cardClicked.innerHTML = "O";
-    } else {
-      cardClicked.innerHTML = "X";
-    }
-    checkForWin();
-    increaseCounter();
-    choosePlayer();
   }
 
   function increaseCounter() {
     counter++;
     console.log("Counter: " + counter);
+    choosePlayer();
   };
 
   function choosePlayer() {
     currentPlayer = counter % 2 != 0 ? 1 : 2;
-    console.log("Current Player:" + currentPlayer);
-    return currentPlayer;
+    console.log("Current player: " + currentPlayer);
   };
 
+  function markCard(cardClicked, boxWithId) {
+    if (currentPlayer === 1) {
+      boxWithId.innerHTML = "X";
+    } else {
+      boxWithId.innerHTML = "O";
+    }
+    storeCard(boxWithId);
+  }
+
+  function storeCard(boxWithId) {
+    if (currentPlayer === 1) {
+      gameController.player1Cards.push(boxWithId.id);
+      console.log("Player 1's cards: ");
+      console.log(gameController.player1Cards);
+    } else {
+      gameController.player2Cards.push(boxWithId.id);
+      console.log("Player 2's cards: ");
+      console.log(gameController.player2Cards);
+    }
+    checkForWin();
+  }
+
   function checkForWin() {
-    
+
   }
 
   return {
     startGame: startGame
   }
+
+})();
+
+var gameController = (function() {
+  var player1Cards = [],
+    player2Cards = [];
+
+  return {
+    player1Cards: player1Cards,
+    player2Cards: player2Cards
+  };
 
 })();
 
